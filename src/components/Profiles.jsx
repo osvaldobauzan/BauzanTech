7087c0fc823ddae1709fa3b99150da604f8bba72
Santiago Pulido Diaz <santiagopulido12@gmail.com>
2024-02-08 14:21:37 +0100

    about added

--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -1,25 +1,252 @@
 'use client';
-import { Card } from 'flowbite-react';
-import { Flowbite } from "flowbite-react";
+import Jenny from '../assets/Jenny_img.jpg'
+import mexflag from '../assets/flag.png'
+import spainflag from '../assets/spain.png'
+import Bauzan from '../assets/bauzan_img.jpg'
+import Santi from '../assets/fotosanti.png'
 import React from 'react'
+import { Modal } from 'flowbite-react';
+import { useState } from 'react';
+import { TiArrowForward } from "react-icons/ti";
 
 const Profiles = () => {
-  return (
-    
 
-<div class="grid grid-cols-1 md:grid-cols-3 gap-4 m-20">
+  const [openModal, setOpenModal] = useState(false);
+  const [openModal2, setOpenModal2] = useState(false);
+  const [openModal3, setOpenModal3] = useState(false);
+  return (
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 xl:my-12">
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" />
+      
+    <div className="flex flex-col items-center py-8">
+      
+        <div className='m-4'>
+        <img src={Bauzan} className='w-32 h-32 mb-3 rounded-full shadow-lg' />
+        </div>
+
+        <h5 className="mb-1 text-xl font-medium text-gray-900">Osvaldo Bauzan</h5>
+        <span className="text-sm text-gray-500 dark:text-gray-400">Fundador y Director Ejecutivo</span>
+        <div className="mt-4 flex space-x-3 lg:mt-6 h-10">
+          <a
+            href="#"
+            className="inline-flex items-center text-center"
+          >
+            <svg className="w-[30px] h-[30px] text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clip-rule="evenodd"/>
+            <path d="M7.2 8.8H4v10.7h3.2V8.8Z"/>
+          </svg>
+          </a>
+          <a
+            href="#"
+            className="inline-flex items-center text-center h-9"
+          >
+          <img src={mexflag} alt="" className='h-6 mt-2' />
+          </a>
+          
+          <button onClick={() => setOpenModal(true)} className='bg-transparent text-black hover:bg-none'><TiArrowForward /></button>
+          <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+            <Modal.Body>
+              <div className="space-y-6">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                Soy un Arquitecto Cloud graduado de Brigham Young University en Ciencias de la Computación. Con varios años de experiencia en el 
+                mercado de soluciones tecnológicas, disfruto aprender idiomas (puedo comunicarme efectivamente en cinco), 
+                me apasiona el ciclismo, los deportes, explorar el mundo y ayudar a los demás. Estoy listo para ayudarte a alcanzar tus objetivos digitales.
+                </p>
+              </div>
+            </Modal.Body>
+          </Modal>
+        </div>
+      </div>
     </div>
     <div>
+    <div className="flex flex-col items-center py-8">
+                <div className='m-4'>
+                  <img src={Santi} className="w-32 h-32 mb-3 rounded-full shadow-lg" />
+                </div>
+
+                <h5 className="mb-1 text-xl font-medium text-gray-900">Santiago Pulido</h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Desarrollador Full Stack</span>
+                <div className="mt-4 flex space-x-3 lg:mt-6 h-10">
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center"
+                  >
+                    <svg className="w-[30px] h-[30px] text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clip-rule="evenodd"/>
+                    <path d="M7.2 8.8H4v10.7h3.2V8.8Z"/>
+                  </svg>
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center h-9"
+                  >
+                  <img src={spainflag} alt="" className='h-6 mt-2' />
+                  </a>
+                  <button onClick={() => setOpenModal2(true)} className='bg-transparent text-black hover:bg-none'><TiArrowForward /></button>
+                    <Modal dismissible show={openModal2} onClose={() => setOpenModal2(false)}>
+                    
+                      <Modal.Body>
+                        <div className="space-y-6">
+                          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                          Actualmente resido en Madrid, España, dedicando mi tiempo completo al desarrollo de Frontend y Backend. Busco entregar calidad en todo lo que desarrollo. 
+                          Me apasiona(...) y estoy listo para materializar tus ideas en código.
+                          </p>
+                        </div>
+                      </Modal.Body>
+                    </Modal>
+                </div>
+              </div>
+    </div>
+    <div>
+    <div className="flex flex-col items-center py-8 ">
+                <div className='m-4'>
+                <img src={Jenny} className='w-32 h-32 mb-3 rounded-full shadow-lg' />
+                </div>
+
+                <h5 className="mb-1 text-xl font-medium text-gray-900">Jennyfer Bauzan Jr</h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Desarrolladora Back End</span>
+                <div className="mt-4 flex space-x-3 lg:mt-6 h-10">
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center"
+                  >
+                    <svg className="w-[30px] h-[30px] text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clip-rule="evenodd"/>
+                    <path d="M7.2 8.8H4v10.7h3.2V8.8Z"/>
+                  </svg>
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center h-9"
+                  >
+                  <img src={mexflag} alt="" className='h-6 mt-2' />
+                  </a>
+                  <button onClick={() => setOpenModal3(true)} className='bg-transparent text-black hover:bg-none'><TiArrowForward /></button>
+                    <Modal dismissible show={openModal3} onClose={() => setOpenModal3(false)}>
+                      <Modal.Body>
+                        <div className="space-y-6">
+                          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                          ¡Hola! Soy Melanie. Estudié Psicología en la Universidad IBERO Puebla y me encargo de asegurar el correcto funcionamiento de todo por aquí. 
+                           Disfruto de mi tiempo libre leyendo y haciendo ejercicio, especialmente yoga, barre y danza latina.
+                           Estaré encantada de explorar cómo podemos hacer crecer tu negocio.
+                          </p>
+                        </div>
+                      </Modal.Body>
+                    </Modal>
+                </div>
+              </div>
+    </div>
+    <div>
+    <div className="flex flex-col items-center py-8 ">
+                <div className='m-4'>
+                <img src={Jenny} className='w-32 h-32 mb-3 rounded-full shadow-lg' />
+                </div>
+
+                <h5 className="mb-1 text-xl font-medium text-gray-900">Jennyfer Bauzan Jr</h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Desarrolladora Back End</span>
+                <div className="mt-4 flex space-x-3 lg:mt-6 h-10">
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center"
+                  >
+                    <svg className="w-[30px] h-[30px] text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clip-rule="evenodd"/>
+                    <path d="M7.2 8.8H4v10.7h3.2V8.8Z"/>
+                  </svg>
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center h-9"
+                  >
+                  <img src={mexflag} alt="" className='h-6 mt-2' />
+                  </a>
+                  <button onClick={() => setOpenModal3(true)} className='bg-transparent text-black hover:bg-none'><TiArrowForward /></button>
+                    <Modal dismissible show={openModal3} onClose={() => setOpenModal3(false)}>
+                      <Modal.Body>
+                        <div className="space-y-6">
+                          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                          ¡Hola! Soy Melanie. Estudié Psicología en la Universidad IBERO Puebla y me encargo de asegurar el correcto funcionamiento de todo por aquí. 
+                           Disfruto de mi tiempo libre leyendo y haciendo ejercicio, especialmente yoga, barre y danza latina.
+                           Estaré encantada de explorar cómo podemos hacer crecer tu negocio.
+                          </p>
+                        </div>
+                      </Modal.Body>
+                    </Modal>
+                </div>
+              </div>
+    </div>
     
-    </div>
+    
 </div>
 
-  )
+
+)
 }
 
 export default Profiles
+
+{/* <div className="grid grid-cols-1 md:grid-cols-3 gap-2 max-w-3xl">
+       
+  <div className="flex justify-center shadow-lg w-full ">
+              
+            </div>             
+            <div className="w-full shadow-lg">
+              <div className="flex flex-col items-center py-8 max-w-5xl ">
+                <div className='h-28 m-4'>
+                <img src={Jenny} className='h-full rounded-full' />
+                </div>
+
+                <h5 className="mb-1 text-xl font-medium text-gray-900">Jennyfer Bauzan Jr</h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                <div className="mt-4 flex space-x-3 lg:mt-6 h-10">
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center"
+                  >
+                    <svg className="w-[30px] h-[30px] text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clip-rule="evenodd"/>
+                    <path d="M7.2 8.8H4v10.7h3.2V8.8Z"/>
+                  </svg>
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center h-9"
+                  >
+                  <img src={mexflag} alt="" className='h-6 my-2' />
+                  </a>
+                </div>
+              </div>
+            </div>             
+            <div className="w-full shadow-lg">
+              <div className="flex flex-col items-center py-8 max-w-5xl ">
+                <div className='h-28 m-4'>
+                <img src={Jenny} className='h-full rounded-full' />
+                </div>
+
+                <h5 className="mb-1 text-xl font-medium text-gray-900">Jennyfer Bauzan Jr</h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                <div className="mt-4 flex space-x-3 lg:mt-6 h-10">
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center"
+                  >
+                    <svg className="w-[30px] h-[30px] text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clip-rule="evenodd"/>
+                    <path d="M7.2 8.8H4v10.7h3.2V8.8Z"/>
+                  </svg>
+                  </a>
+                  <a
+                    href="#"
+                    className="inline-flex items-center text-center h-9"
+                  >
+                  <img src={mexflag} alt="" className='h-6 my-2' />
+                  </a>
+                </div>
+              </div>
+            </div>             
+</div> */}
+
+
 
 // import React from 'react'
 // import { Carousel, Card } from 'flowbite-react';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,11 @@ import Nav from '../components/Navbar'
 const Home = () => {
   return (
    
-   
-    <section className="font-Montserrat font-bold">
+    <section className="grid font-Montserrat font-bold">
         <Nav logo={BuazanRoundedLogo}  />
-		<div className='h-4'></div>
-    	<div className=" text-green-bauzan container flex flex-col justify-center md:flex-col p- mx-auto max-h-screen xs sm:py-12 lg:py-2 lg:px-10 lg:flex-row lg:justify-between xl:py-20 xl:text-2xl">
-		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left lg:text-lg xl:text-2xl">
+		<div className='h-8 lg:h-2'></div>
+    <div className=" text-green-bauzan container mt-6 flex flex-col justify-center md:flex-col mx-auto max-h-screen sm:py-8 lg:py-0 lg:px-10 lg:flex-row lg:gap-10 xl:py-12 xl:text-2xl">
+		<div className="flex flex-col justify-center px-6 pb-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left lg:text-lg xl:text-2xl">
             <div className='transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-700'>
             <h1 className="font-Flipahaus text-6xl font-bold leadi sm:text-6xl">Welcome
 				<span className="text-blue-bauzan"> to </span>
@@ -34,12 +33,12 @@ const Home = () => {
 			<img src={imageHero} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-700" />
 		</div>
 	</div>
-
-	<footer className="bg-white rounded-lg max-h-full mt-4 sm:flex sm:flex-col sm:text-center-center sm:justify-between">
-		<div className="w-full max-w-screen-xl mx-auto  md:py-8 lg:px-8">
-			<div className="lg:flex lg:flex-row lg:justify-between sm:flex sm:flex-row sm:justify-between flex flex-col items-center space-y-7">
-				<a href="https://www.bauzantech.com/" className="flex w-auto items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-					<img src={BauzanRoundedRedLogo} className="h-12" />
+	<div className='h-2'></div>
+	<footer className="bg-white rounded-lg max-h-full mt-4 sm:flex sm:flex-col sm:text-center sm:justify-between ">
+		<div className="w-full max-w-screen-xl mx-auto md:py-8 lg:px-8">
+			<div className="lg:flex lg:flex-row lg:justify-between sm:flex sm:flex-row sm:justify-between flex flex-col items-center space-y-7 mt-4 md:mt-0">
+				<a href="https://www.bauzantech.com/" className="flex w-auto items-center mb-4  space-x-3 rtl:space-x-reverse">
+					<img src={BauzanRoundedRedLogo} className="h-12 sm:hidden md:flex" />
 				</a>
 				<ul className="flex flex-wrap items-center mb-6 text-xs font-semibold text-gray-500 sm:mb-0 ">
 					<span className="block text-sm text-gray-500 sm:text-center ">© 2024 <a href="https://www.bauzantech.com/" className="hover:underline">Bauzan Tech Solutions</a>. All Rights Reserved.</span>
