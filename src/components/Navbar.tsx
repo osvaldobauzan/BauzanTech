902d4325bcf978d29a85679fa153b25c33262b7e
Osvaldo <osvaldo.bauzan@gmail.com>
2024-02-09 20:23:52 +0100

    acento2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Nav = ({ logo }) => {
 
         <Navbar.Link href="#"  className="flex font-bold items-center px-4 mb-1 xl:text-xl hover:text-green-bauzan hover:border-b-2-green-bauzan transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-300">Clientes</Navbar.Link>
         <Link to={'/contact'}>
-        <Navbar.Link href="#"  className="flex font-bold items-center px-4 mb-1 xl:text-xl hover:text-green-bauzan hover:border-b-2-green-bauzan transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-300">Contactanos</Navbar.Link>
+        <Navbar.Link href="#"  className="flex font-bold items-center px-4 mb-1 xl:text-xl hover:text-green-bauzan hover:border-b-2-green-bauzan transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-300">Contáctanos</Navbar.Link>
         </Link>
       </Navbar.Collapse>
     </Navbar>
